8ae019dd84d8cc65fe0ce7404856b733be21b07b
Lazar Dondo <ldondo@s4hc.com>
2022-04-19 13:56:16 +0200

    Add delete tournament endpoint

---
+++ atp-tour/src/handlers/tournament/deleteTournament.js
@@ -0,0 +1,29 @@
+import AWS from 'aws-sdk';
+import commonMiddleware from '../../lib/commonMiddleware';
+import createError from 'http-errors';
+import { getTournamentById } from './getTournament';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function deleteTournament(event, context){
+    const {id} = event.pathParameters;
+    const tournament = getTournamentById(id);
+
+    const params = {
+        TableName: process.env.TOURNAMENT_TABLE_NAME,
+        Key: {id}
+    };
+
+    try{
+        await dynamodb.delete(params).promise();
+        return {
+            statusCode: 200,
+            body: 'Tournament deleted successfully'
+        }
+    }
+    catch(error){
+        throw new createError.InternalServerError(error);
+    }
+}
+
+export const handler = commonMiddleware(deleteTournament);
