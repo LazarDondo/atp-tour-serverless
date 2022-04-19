cbafc26ec499876c44a2d0b57c067ab39a63c058
Lazar Dondo <ldondo@s4hc.com>
2022-04-19 12:52:49 +0200

    Add get all tournaments endpoint

---
+++ atp-tour/src/handlers/tournament/getTournaments.js
@@ -0,0 +1,27 @@
+import AWS from 'aws-sdk';
+import commonMiddleware from '../../lib/commonMiddleware';
+import createError from 'http-errors';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+export async function getTournaments(event, context){
+    let tournaments;
+    
+    try{
+        const result = await dynamodb.scan({
+            TableName: process.env.TOURNAMENT_TABLE_NAME
+        }).promise();
+        tournaments = result.Items;
+    }
+    catch(error){
+        console.log(error);
+        throw new createError.InternalServerError(error);
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.parse(tournaments)
+    }
+}
+
+export const handler = commonMiddleware(tournaments);
