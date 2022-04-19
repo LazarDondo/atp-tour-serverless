c4659473c91fe794b4a2f6aab8af8dc51f34660d
Lazar Dondo <ldondo@s4hc.com>
2022-04-19 11:16:25 +0200

    Expose create tournament endpoint

---
+++ atp-tour/src/handlers/tournament/create.js
@@ -0,0 +1,44 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import commonMiddleware from '../../lib/commonMiddleware';
+import createError from 'http-errors';
+import validator from '@middy/validator';
+import createTournamentSchema from '../../lib/schemas/createTournamentSchema';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+async function createTournament(event, context) {
+    const { name, startDate, hostCountry, tournamentType } = event.body;
+
+    let completionDate = new Date(startDate);
+    completionDate.setDate(completionDate.getDate()+7);
+
+    const tournament = {
+        id: uuid(),
+        name,
+        startDate,
+        completionDate: completionDate.toISOString().split('T')[0],
+        hostCountry,
+        tournamentType
+    }
+
+    try{
+    await dynamodb.put({
+        TableName: process.env.TOURNAMENT_TABLE_NAME,
+        Item: tournament
+    }).promise();
+    }
+    catch(error){
+        console.log(error);
+        throw new createError.InternalServerError(error);
+    }
+    return {
+        statusCode: 201,
+        body: JSON.stringify(tournament),
+    };
+}
+
+export const handler = commonMiddleware(createTournament)
+.use(validator({
+    inputSchema: createTournamentSchema
+}));
---
+++ atp-tour/src/lib/schemas/createTournamentSchema.js
@@ -0,0 +1,29 @@
+const schema = {
+    type: 'object',
+    properties:{
+        body:{
+            type:'object',
+            properties: {
+                name: {
+                    type: 'string'
+                },
+                startDate: {
+                    type: 'string',
+                    format: 'date'
+                },
+                hostCountry: {
+                    type: 'number'
+                },
+                tournamentType: {
+                    type: 'string',
+                    enum: ['Grand Slam', 'Masters 1000'],
+                    default: 'Grand Slam'
+                }
+            },
+            required: ['name', 'startDate', 'hostCountry']
+        }
+    },
+    required: ['body']
+};
+
+export default schema;
